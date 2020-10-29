f5b0c920fda04b2c21ea5cb022144bd68a257bef
Diana Kyrychyk <d.rudnytska@gmail.com>
2020-10-29 10:25:30 +0200

    feat: Add success balance recharge message #common

--- src/main/java/ua/com/taxi/controller/BalanceController.java
+++ src/main/java/ua/com/taxi/controller/BalanceController.java
@@ -1,6 +1,7 @@
 package ua.com.taxi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,18 +9,23 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ua.com.taxi.domain.dto.user.RechargeBalance;
 import ua.com.taxi.service.UserService;
 
 import java.security.Principal;
+import java.util.Locale;
 
 @Controller
 public class BalanceController {
 
+    private MessageSource messageSource;
+
     private UserService userService;
 
     @Autowired
-    public BalanceController(UserService userService) {
+    public BalanceController(MessageSource messageSource, UserService userService) {
+        this.messageSource = messageSource;
         this.userService = userService;
     }
 
@@ -32,14 +38,17 @@ public class BalanceController {
 
     @PostMapping("/user/balance-recharge")
     public String updateUserBalance(@Validated @ModelAttribute("rechargeBalance") RechargeBalance rechargeRequest,
-                                    BindingResult bindingResult, Principal principal) {
+                                    BindingResult bindingResult, Principal principal, RedirectAttributes redirectAttributes, Locale locale) {
 
         String page = "";
         if (bindingResult.hasErrors()) {
             page = "/user/balance-recharge";
         } else {
             userService.rechargeBalance(principal.getName(), rechargeRequest.getAmount());
-            page = "redirect:/";
+
+            String message = messageSource.getMessage("balance-recharge.success-message", new Object[0], locale);
+            redirectAttributes.addFlashAttribute("successMessage", message);
+            page = String.format("redirect:/user/balance-recharge?amount=%s", rechargeRequest.getAmount());
         }
         return page;
     }
